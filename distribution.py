f7e4a895f71f8a86444ff4f7571def3350adc796
castlop <castlop.ma@gmail.com>
2021-06-29 21:19:37 -0500

    Feat: Convert imported trip data to  dataframes
    Pass data related to zones, trips and restrictions and convert to two
    dataframes as follows:
    * trips and zones: use zones as index and columns for trip data
    * restrictions: includes column names by passing a dictionary

--- distribution.py
+++ distribution.py
@@ -11,6 +11,13 @@ def import_json(filepath):
 def get_file_location():
     return pathlib.Path(input('Enter file path: '))
 
+def convert_to_dataframes(zones, trips, restrictions):
+    return [pd.DataFrame(base_trips, index=base_zones, columns=base_zones),
+            pd.DataFrame(base_restrictions, index=base_zones)]
+
 if __name__ == '__main__':
     filepath = get_file_location()
     base_zones, base_trips, base_restrictions = import_json(filepath).values()
+    trips, restrictions = convert_to_dataframes(base_zones,
+                                                base_trips,
+                                                base_restrictions)
