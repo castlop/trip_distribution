738be9493562c6a2000c3b337c5d98600e691cc1
castlop <castlop.ma@gmail.com>
2021-06-29 22:53:18 -0500

    Feat: Add algorithm to distribute trips
    The algorithm follows next steps:
    1) Take a base trips matrix
    2) Sum by rows
    3) Calculate origins expansion factors
    4) Multiply Matrix by origins expansion factors
    5) Repeat steps 2 to 4 but for columns and destinies
    
    In the end, return a balanced matrix with distributed trips and
    full satisfied restrictions.

--- distribution.py
+++ distribution.py
@@ -15,6 +15,18 @@ def convert_to_dataframes(zones, trips, restrictions):
     return [pd.DataFrame(base_trips, index=base_zones, columns=base_zones),
             pd.DataFrame(base_restrictions, index=base_zones)]
 
+def distribute_trips(affected_trips, restrictions):
+    pivot_origins = affected_trips.sum(axis='columns')
+    origins_expansion_factors = restrictions['origins'].div(pivot_origins, axis='index')
+    affected_trips = affected_trips.mul(origins_expansion_factors, axis='index')
+    
+    pivot_destinies = affected_trips.sum(axis='index')
+    destinies_expansion_factors = restrictions['destinies'].div(pivot_destinies, axis='index')
+    affected_trips = affected_trips.mul(destinies_expansion_factors, axis='columns')
+    
+    return affected_trips
+
+
 if __name__ == '__main__':
     filepath = get_file_location()
     base_zones, base_trips, base_restrictions = import_json(filepath).values()
@@ -22,3 +34,4 @@ if __name__ == '__main__':
                                                 base_trips,
                                                 base_restrictions)
     affected_trips = trips.copy()
+    distributed_trips = distribute_trips(affected_trips, restrictions)
