680530cb72eeb35616fe3af4698b9691abf858cc
castlop <castlop.ma@gmail.com>
2021-06-29 20:50:02 -0500

    Feat: Get file location from user

--- distribution.py
+++ distribution.py
@@ -7,3 +7,6 @@ def import_json(filepath):
     with open(filepath, "r") as read_file:
         data =  json.load(read_file)
     return data
+
+def get_file_location():
+    return pathlib.Path(input('Enter file path: '))
