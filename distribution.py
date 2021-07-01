5c1e35ca59d9612241cc42419067aa0468efcc82
castlop <castlop.ma@gmail.com>
2021-06-30 21:45:09 -0500

    Feat: Convert results to dictionaries
    Results from trip distribution are collected and converted in a
    dictionary to make JSON transformation easier.

--- distribution.py
+++ distribution.py
@@ -15,6 +15,16 @@ def convert_to_dataframes(zones, trips, restrictions):
     return [pd.DataFrame(base_trips, index=base_zones, columns=base_zones),
             pd.DataFrame(base_restrictions, index=base_zones)]
 
+def prepare_export_data(trips, restrictions):
+    return {
+        'zones': trips.index.tolist(),
+        'trips': trips.values.tolist(),
+        'restrictions': {
+            'origins': trips.sum(axis="columns").values.tolist(),
+            'destinies': trips.sum(axis="index").values.tolist()
+        }
+    }
+
 def distribute_trips(affected_trips, restrictions):
     pivot_origins = affected_trips.sum(axis='columns')
     origins_expansion_factors = restrictions['origins'].div(pivot_origins, axis='index')
@@ -35,3 +45,4 @@ if __name__ == '__main__':
                                                 base_restrictions)
     affected_trips = trips.copy()
     distributed_trips = distribute_trips(affected_trips, restrictions)
+    export_data = prepare_export_data(distributed_trips, restrictions)
